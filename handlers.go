5e38870e7bdcc93d65fd84e49540e6cf540a57ec
Stisin <stanislav_zolotarev@mail.ru>
2022-08-31 16:53:09 +0300

    blockchain

--- handlers.go
+++ handlers.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func getBlockchainHandler(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
+}
+
+type Message struct {
+	BPM int
+}
+
+func writeBlockHandler(w http.ResponseWriter, r *http.Request) {
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, )
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", getBlockchainHandler).Methods("GET")
+	router.HandleFunc("/", writeBlockHandler).Methods("POST")
+
 	err := godotenv.Load("serverconf.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -17,9 +22,15 @@ func main() {
 	httpPort := ":" + os.Getenv("PORT")
 	log.Println("Listening on ", httpPort)
 
-	http.ListenAndServe(httpPort, nil)
+	server := &http.Server{
+		Addr:           httpPort,
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", getBlockchainHandler).Methods("GET")
-	router.HandleFunc("/", writeBlockHandler).Methods("POST")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
